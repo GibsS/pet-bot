306c9d1323d01a5422ee28a4719e5c46bafbbd0e
Peter Worth <peterworth@gmail.com>
2017-06-17 16:37:35 +0100

    sh->bash

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/peterworth/pet-bot/
 python translate.py --data_dir train/ --train_dir train/ --from_train_data data/conversation_prompt.txt --to_train_data data/conversation_reply.txt
