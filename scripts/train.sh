748e814187471c4618bb15588c97c89d015ba42c
Peter Worth <peterworth@gmail.com>
2017-06-17 16:35:06 +0100

    train script executable

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+cd /home/peterworth/pet-bot/
 python translate.py --data_dir train/ --train_dir train/ --from_train_data data/conversation_prompt.txt --to_train_data data/conversation_reply.txt
